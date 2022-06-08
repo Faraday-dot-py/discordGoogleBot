24fd6de759c876debb160ff04fb7ab9719d66b00
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-06-07 22:07:57 -0700

    Got a basic google bot working with !google and !help

--- googleBot.py
+++ googleBot.py
@@ -1,10 +1,12 @@
 import os
 
 import discord
+import wikipedia.exceptions
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv("OTgzOTU0MzU4NjI0NDgxMjgw.GpaQjs.S9EwxLo_01aoo3mQDN5LdIon9d6BjD6lfUVyX4")
+import wikipedia as wk
+
+from DISCORD_TOKEN import DISCORD_TOKEN
 
 client = discord.Client()
 
@@ -12,4 +14,25 @@ client = discord.Client()
 async def on_ready():
     print(f"{client.user} has connected to Discord!")
 
-client.run("OTgzOTU0MzU4NjI0NDgxMjgw.GpaQjs.S9EwxLo_01aoo3mQDN5LdIon9d6BjD6lfUVyX4")
+@client.event
+async def on_message(message):
+    print("Got response")
+
+    if message.author == client.user:
+        return
+
+    if message.content == "!help":
+        await message.channel.send("Use the command: !google [search term] to look up stuff.\nUse the command: !help to display this message")
+
+    if message.content.find("!google") == 0:
+        try:
+            response = wk.summary(message.content[7:], sentences = 3)
+
+            await message.channel.send(response)
+
+        except wikipedia.exceptions.PageError:
+            await message.channel.send("Was not able to find a page on {}".format(message.content[7:]))
+
+
+
+client.run(DISCORD_TOKEN)
